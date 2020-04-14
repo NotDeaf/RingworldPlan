a91cb6bb1642d513ab8826ce87a32016ab38a3b8
grudnyai22 <47308066+grudnyai22@users.noreply.github.com>
2020-04-14 12:11:44 -0400

    Add files via upload

---
+++ Ringworld.java
@@ -0,0 +1,16 @@
+/**
+ * Ringworld is a class that constructs data that simulates
+ *     a Ringworld, which is a ring that orbits around a star
+ *     that is comprised of all of the planets in the universe.
+ *     Ringworld uses two auxiliary classes, Planet and Converter,
+ *     to make conversion tasks and planet data easy to access.
+ *
+ * @author Luke Pastore
+ * @author Ansh Motiani
+ * @author Gar Rudnyai
+ * @version April 14, 2020
+ */
+public class Ringworld
+{
+    
+}
