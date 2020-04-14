ec1b50fc8e179b3fc09b1c6fe8ff5f053f1ee376
grudnyai22 <47308066+grudnyai22@users.noreply.github.com>
2020-04-14 11:52:39 -0400

    Update Converter.java

--- RingworldProject/Converter.java
+++ RingworldProject/Converter.java
@@ -1,20 +1,44 @@
+/**
+ * Converter is an auxiliary class to Planet and Ringworld which is
+ *     converts between units to control the units of the calculations
+ *     within the Planet and Ringworld classes
+ *
+ * @author Luke Pastore
+ * @author Ansh Motiani
+ * @author Gar Rudnyai
+ * @version April 14, 2020
+ */
 public class Converter
 {
     private double currentValue;
-    private double final FEETSQRD_TO_MILESQRD = 3.58701 * Math.pow(10, -8);
-    private double final FEETCUBED_TO_MILECUBED = 6.79357 * Math.pow(10, -8);
+    private final static double 
+        FEETSQRD_TO_MILESQRD = 3.58701 * Math.pow(10, -8);
+    private final static double 
+        FEETCUBED_TO_MILECUBED = 6.79357 * Math.pow(10, -8);
     
+    /**
+     * Constructor for Converter objects
+     * @param current current value
+     */
     public Converter(double current)
     {
         this.currentValue = current;
     }
     
-    public double ft2ToMi2(double feetSquared)
+    /**
+     * Converts a measurement in feet squared to sqaure miles
+     * @param feetSquared measurement in feet squared
+     */
+    public static double ft2ToMi2(double feetSquared)
     {
         return feetSquared * FEETSQRD_TO_MILESQRD;
     }
     
-    public double ft2ToMi2(double feetCubed)
+    /**
+     * Converts a measurement in feet cubed to miles cubed
+     * @param feetCubed measurement in feet cubed
+     */
+    public static double ft3ToMi3(double feetCubed)
     {
         return feetCubed * FEETCUBED_TO_MILECUBED;
     }
