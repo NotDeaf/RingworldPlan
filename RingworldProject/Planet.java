cae76c976b5e5fafa68a06106aaa2d6646bc9892
grudnyai22 <47308066+grudnyai22@users.noreply.github.com>
2020-04-14 12:02:26 -0400

    Update Planet.java

--- RingworldProject/Planet.java
+++ RingworldProject/Planet.java
@@ -10,49 +10,49 @@
 public class Planet
 {
     // Instance Variables
-    private double radius;
-    private double volume;
-    private double surfaceArea;
+    private double radiusFt;
+    private double volumeFt3;
+    private double surfaceAreaFt2;
     
     // Our Universe's Planet's Volume in Cubic Feet
-    private final double JUPITER_VOLUME = 1.43128 * Math.pow(10,15);
-    private final double SATURN_VOLUME = 8.2713 * Math.pow(10,14);
-    private final double URANUS_VOLUME = 6.833 * Math.pow(10,13);
-    private final double NEPTUNE_VOLUME = 6.254 * Math.pow(10 ,13);
-    private final double EARTH_VOLUME = 1.08321 * Math.pow(10,12);
-    private final double VENUS_VOLUME = 9.2843 * Math.pow(10,11);
-    private final double MARS_VOLUME = 1.6318 * Math.pow(10,11);
-    private final double MERCURY_VOLUME = 6.083 * Math.pow(10,10);
+    private final double JUPITER_VOLUME_FT3 = 1.43128 * Math.pow(10,15);
+    private final double SATURN_VOLUME_FT3 = 8.2713 * Math.pow(10,14);
+    private final double URANUS_VOLUME_FT3 = 6.833 * Math.pow(10,13);
+    private final double NEPTUNE_VOLUME_FT3 = 6.254 * Math.pow(10 ,13);
+    private final double EARTH_VOLUME_FT3 = 1.08321 * Math.pow(10,12);
+    private final double VENUS_VOLUME_FT3 = 9.2843 * Math.pow(10,11);
+    private final double MARS_VOLUME_FT3 = 1.6318 * Math.pow(10,11);
+    private final double MERCURY_VOLUME_FT3 = 6.083 * Math.pow(10,10);
     
     /**
      * No Parameter Planet constructor
      */
     public Planet()
     {
-        this.radius = 0;
-        this.volume = 0;
-        this.surfaceArea = 0;
+        this.radiusFt = 0;
+        this.volumeFt3 = 0;
+        this.surfaceAreaFt2 = 0;
     }
     
     /**
      * Planet constructor given the radius of the planet
-     * @param radius the radius of the planet, in 
+     * @param radius the radius of the planet, in feet
      */
-    public Planet(double radius)
+    public Planet(double radiusFt)
     {
-        this.radius = radius;
-        this.volume = 2 * radius * Math.PI;
-        this.surfaceArea = 4 * Math.pow(radius, 2) * Math.PI;
+        this.radiusFt = radiusFt;
+        this.volumeFt3 = 2 * radiusFt * Math.PI;
+        this.surfaceAreaFt2 = 4 * Math.pow(radiusFt, 2) * Math.PI;
     }
     
     /**
      * Adds a given amount of volume, assuming that the planet is
      *     always perfectly spherical
-     * @param double quantity
+     * @param double quantity in feet cubed
      */
     public void addVolume(double quantity)
     {
-        volume += quantity;
+        volumeFt3 += quantity;
     }
     
     /**
@@ -61,8 +61,7 @@ public class Planet
      */
     public double getVolume()
     {
-        Converter current = new Converter(volume);
-        return current.ft3ToMi3();
+        return Converter.ft3ToMi3(volumeFt3);
     }
     
     
@@ -72,7 +71,6 @@ public class Planet
      */
     public double getSurfaceArea()
     {
-        Converter current = new Converter(surfaceArea);
-        return current.ft2ToMiles2();
+        return Converter.ft2ToMi2(surfaceAreaFt2);
     }
 }
